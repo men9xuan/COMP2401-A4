1d68ca69cf68f046745008bac9d57c7be74ba153
men9xuan <44915783+men9xuan@users.noreply.github.com>
2022-05-02 11:19:25 -0400

    Delete orig directory

--- orig/display.h
+++
@@ -1,8 +0,0 @@
-#define  ENV_WIDTH       740
-#define  ENV_HEIGHT      540
-
-
-// These are the external functions being used by the test program
-extern void initializeWindow() ;
-extern void closeWindow();
-extern void displayEnvironment(Environment *);
